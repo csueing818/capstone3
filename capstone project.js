a31785a1a957c0ff7e81334a62fe5b705d12b04f
csueing818 <122180271+csueing818@users.noreply.github.com>
2023-03-25 08:42:33 -0700

    Add files via upload

--- capstone project.js
+++ capstone project.js
@@ -1,32 +1,46 @@
-alert('You successfully linked your JavaScript file!');
-$('document').ready(function(){
-	$('input[type="text"], input[type="email"], textarea').focus(function(){
+
+$(document).ready(function(){
+	$('input[type="name"], input[type="email"], textarea').focus(function(){
 		var background = $(this).attr('id');
 		$('#' + background + '-form').addClass('formgroup-active');
 		$('#' + background + '-form').removeClass('formgroup-error');
 	});
-	$('input[type="text"], input[type="email"], textarea').blur(function(){
+	$('input[type="name"], input[type="email"], textarea').blur(function(){
 		var background = $(this).attr('id');
 		$('#' + background + '-form').removeClass('formgroup-active');
 	});
 
-function errorfield(field){
-	$(field).addClass('formgroup-error');
-	console.log(field);	
-}
+	function errorfield(field){
+		$(field).addClass('formgroup-error')
+		console.log(field);	
+	}
+	$("#waterform").submit(function(event) {
+		var stopsubmit = false;
+		const waterform = $("#waterform")[0].checkValidity();
+		console.log(waterform[0]);
+		
+	
+	if(!$('#name').val()) {
+			errorfield('#name-form');
+			stopsubmit=true;
+			console.log("name field is empty");
+		}
+		else if ($('#name').val().length <3){
+			errorfield('#name-form');
+			stopsubmit=true;
+			console.log("name field is test characters");
+		}
 
-$("#waterform").submit(function() {
-	var stopsubmit = false;
+		if(!$('#email').val()) {
+			errorfield('#email-form');
+			stopsubmit=true;
+		}
+		if(stopsubmit) {
+			event.preventDefault();
+			alert("Please fill out the name,email, and message section!");
+			return false;
+		}
 
-if($('#name').val() == "") {
-	errorfield('#name-form');
-	stopsubmit=true;
-}
-if($('#email').val() == "") {
-	errorfield('#email-form');
-	stopsubmit=true;
-}
-if(stopsubmit) return false;
-});	
+		});
+	});
 
-});
